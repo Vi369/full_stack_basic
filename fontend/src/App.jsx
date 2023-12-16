90ccd89cee1183a0ac2bf6571f1f57a623c5dda0
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-16 16:27:20 +0530

    complete application

---
+++ fontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((res)=>{
+      setJokes(res.data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  })
+
+  return (
+    <>
+    <h1>Chai and full stack || youtube</h1>
+    <p>jokes: {jokes.length}</p>
+    {
+      jokes.map((joke)=>{
+       return <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      })
+    }
+    </>
+  )
+}
+
+export default App
